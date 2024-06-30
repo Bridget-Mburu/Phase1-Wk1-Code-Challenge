93c0759f69373245a1eb782747a0a8192006e73c
bridget <bridget.mburu.w@gmail.com>
2024-06-30 12:35:20 +0300

    modified grade generator

---
+++ netSalary.js
@@ -0,0 +1,12 @@
+//Net Salary Calculator
+
+ 
+ let basicSalary = 456000 
+ let deductions = 
+ 
+    
+ if(salary > 800000){
+    console.log(`${salary*35/100}`)
+ } else if (salary <=800000 && salary >500001){
+    console.log(`${salary}`)
+ }
